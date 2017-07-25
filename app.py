31ee33f30c5cb8686d30ee2c1853999b0959268a
oyty <cooloyty@gmail.com>
2017-07-25 11:18:16 +0800

    commit app handle

---
+++ app.py
@@ -0,0 +1,12 @@
+__author__ = 'oyty'
+
+import web
+from handle import Handle
+
+urls = (
+    '/wx', 'Handle',
+)
+
+if __name__ == '__main__':
+    app = web.application(urls, globals())
+    app.run()
