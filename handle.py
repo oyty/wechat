64238dd55450f9f106c1b23c30afabd403bc02f3
oyty <cooloyty@gmail.com>
2017-07-27 12:02:29 +0800

    commit changes

--- handle.py
+++ handle.py
@@ -5,24 +5,31 @@ import hashlib
 import web
 import receive
 import reply
-
+import MySQLdb as mdb
 
 class Handle(object):
     def POST(self):
+        con = None
         try:
             webData = web.data()
             print "Handle Post webdata is ", webData   #后台打日志
             recMsg = receive.parse_xml(webData)
+            con = mdb.connect(host='localhost', user='root', passwd='892968', db='oyty', charset='utf8')
+            cur = con.cursor()
+            cur.execute("SELECT * FROM poem WHERE poem LIKE '%" + recMsg.Content + "%'")
+            poems = cur.fetchall()
             if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
                 toUser = recMsg.FromUserName
                 fromUser = recMsg.ToUserName
-                content = "海上生明月，天涯共此时"
+                content = poems[0][1]
                 replyMsg = reply.TextMsg(toUser, fromUser, content)
                 return replyMsg.send()
             else:
                 print "暂且不处理"
                 return "success"
         except Exception, Argment:
+            if con:
+                con.close()
             return Argment
 
     def GET(self):
